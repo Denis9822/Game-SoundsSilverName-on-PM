538132b9f7210ef172f2bd132fe68c6594f612f7
Denis <ss.set2013@yandex.ru>
2017-11-23 12:55:55 +0200

    NEW

---
+++ Assets/ClickOnButton.cs
@@ -0,0 +1,103 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.Advertisements;
+
+public class ClickOnButton : MonoBehaviour {
+	
+	public static int AdsCount = 0;
+
+	public int i;
+	public float size;
+
+	public Color start;
+	public Color end;
+
+	public GameObject[] ass;
+	private GameObject musobj;
+
+	private bool iSCLickOnButton;
+	private bool iSAds;
+
+	void Start()
+	{
+
+
+
+
+
+		iSAds = true;
+		if (Advertisement.isSupported) 
+			Advertisement.Initialize ("1584613",false);
+
+		i = 0;
+		ass = new GameObject[2000];
+
+	}
+
+
+
+
+	public void SizeButton(string name )
+	{
+		ass[i] = GameObject.Find (name);
+			ass[i].GetComponent<Image> ().color = end;
+		i++;
+		AdsCount++;
+	}
+
+	public void Musics(string name1)
+	{
+		musobj = GameObject.Find (name1);
+			musobj.GetComponent<AudioSource>().Play ();
+		Debug.Log (musobj.GetComponent<AudioSource> ().time);
+		iSCLickOnButton = true;
+
+	
+
+
+	}
+
+	public void ParticlNum(string name2)
+	{
+		GameObject.Find (name2).GetComponent<ParticleSystem> ().Play ();
+	}
+
+
+	void Update()
+	{
+
+		if (iSCLickOnButton == true) {
+			
+			if (musobj.GetComponent<AudioSource> ().isPlaying == false) {
+
+				for (int j = 0; j < i; j++) {
+					ass [j].GetComponent<Image> ().color = start;
+
+				}
+				iSCLickOnButton = false;
+				i = 0;
+			}
+		
+		}
+
+
+		if (iSCLickOnButton == false && AdsCount / 10 == 1) {
+			StartCoroutine ("ShowADS");
+
+			AdsCount = 0;
+		}
+	}
+
+	IEnumerator ShowADS(){
+
+		yield return new WaitForSeconds (0.01f);
+		while (!Advertisement.IsReady ())
+			yield return null;
+		Advertisement.Show ();
+
+	}
+
+
+}
---
+++ Assets/CmPos.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CmPos : MonoBehaviour {
+	
+	public Vector2 startpos;
+	public Camera cam;
+	public GameObject p3;
+
+	public GameObject[] arr;
+
+	public int i;
+	// Use this for initialization
+	void Start () {
+
+		arr = new GameObject[5];
+
+		i = 0;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetMouseButtonDown (0) && i<5) {
+			startpos = cam.ScreenToWorldPoint (Input.mousePosition);
+
+			arr [i] = Instantiate (p3, startpos, Quaternion.identity);
+			i++;
+			StartCoroutine (dests());
+			Debug.Log (arr.Length);
+		}
+	}
+
+	IEnumerator dests ()
+	{
+
+		yield return new WaitForSeconds(3f);
+
+
+
+		Destroy (GameObject.Find ("p3(Clone)"));
+		i--;
+
+	}
+}
